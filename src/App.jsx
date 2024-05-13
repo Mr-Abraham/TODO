19126f5b0aa2f2ff3baca159612094fc740a7e1c
Shivam Abraham <codingnerd84@gmail.com>
2024-05-14 00:57:30 +0530

    localstorage functionality added

--- src/App.jsx
+++ src/App.jsx
@@ -5,22 +5,25 @@ import { TodoContext } from "./store/Context";
 import Task from "./components/Task";
 import TaskList from "./components/TaskList";
 import Info from "./components/Info";
+import { GiJasonMask } from "react-icons/gi";
 
 function taskReducrer(curr, action) {
   let newTaskList = curr;
   if (action.type === "NEW_TASK") {
     newTaskList = [
-      ...curr,
       {
         id: action.payload.id,
         taskName: action.payload.taskName,
         hasCompleted: action.payload.hasCompleted,
       },
+      ...curr,
     ];
+    localStorage.setItem("todo", JSON.stringify(newTaskList));
   } else if (action.type === "DELETE") {
     newTaskList = curr.filter(
       (item) => item.taskName !== action.payload.taskName
     );
+    localStorage.setItem("todo", JSON.stringify(newTaskList));
   } else if (action.type === "UPDATE") {
     newTaskList = curr.map((item) => {
       if (item.taskName == action.payload.taskName) {
@@ -34,12 +37,15 @@ function taskReducrer(curr, action) {
     newTaskList = curr.filter((item) => !item.hasCompleted);
   } else if (action.type == "SHOW_ACTIVE") {
     newTaskList = curr.filter((item) => !item.hasCompleted);
+  } else if (action.type == "STR_TASK") {
+    newTaskList = action.payload;
   }
   return newTaskList;
 }
 function App() {
   const [lightMode, setlightMode] = useState(true);
   const [todo, dispatchTask] = useReducer(taskReducrer, []);
+
   const addTodo = (newTask) => {
     const newTaskAction = {
       type: "NEW_TASK",
@@ -51,7 +57,16 @@ function App() {
     };
     dispatchTask(newTaskAction);
   };
-
+  useEffect(() => {
+    const storedTodos = JSON.parse(localStorage.getItem("todo"));
+    if (storedTodos && storedTodos.length > 0) {
+      const storedTodo = {
+        type: "STR_TASK",
+        payload: storedTodos,
+      };
+      dispatchTask(storedTodo);
+    }
+  }, []);
   const deleteTodo = (task) => {
     const deleteItemAction = {
       type: "DELETE",
